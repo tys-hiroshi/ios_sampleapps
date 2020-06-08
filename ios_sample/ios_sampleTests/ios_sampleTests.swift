7e00390152c1af268c72131e3753c87161935276
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-06-08 23:56:05 +0900

    add

---
+++ ios_sample/ios_sampleTests/ios_sampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_sampleTests.swift
+//  ios_sampleTests
+//
+//  Created by macmini on 2020/06/06.
+//  Copyright © 2020 hblockth. All rights reserved.
+//
+
+import XCTest
+@testable import ios_sample
+
+class ios_sampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
