f9084bd8d45a8aaf629159c2a6d51dccce3641f9
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-06-20 23:09:09 +0900

    https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/DefineYourDataModel.html#//apple_ref/doc/uid/TP40015214-CH20-SW1

--- ios_sampleapp/ios_sampleapp/Meal.swift
+++ ios_sampleapp/ios_sampleapp/Meal.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class Meal {
+public class Meal {
     
     //MARK: Properties
     
@@ -19,16 +19,21 @@ class Meal {
     //MARK: Initialization
      
     init?(name: String, photo: UIImage?, rating: Int) {
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        // The name must not be empty
+        guard !name.isEmpty else {
+            return nil
+        }
+         
+        // The rating must be between 0 and 5 inclusively
+        guard (rating >= 0) && (rating <= 5) else {
+            return nil
+        }
+
         // Initialize stored properties.
         self.name = name
         self.photo = photo
         self.rating = rating
-        
-        // Initialization should fail if there is no name or if the rating is negative.
-        if name.isEmpty || rating < 0  {
-            return nil
-        }
     }
-    
-    
 }
