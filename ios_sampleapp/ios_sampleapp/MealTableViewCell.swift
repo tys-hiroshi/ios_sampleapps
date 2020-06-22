1b117a685d18709b3bf41c1b23c48259285c29c3
tys-hiroshi <tashiro.hiroshi@toyoko-sys.co.jp>
2020-06-22 23:27:30 +0900

    add controller
    https://developer.apple.com/library/archive/referencelibrary/GettingStarted/DevelopiOSAppsSwift/CreateATableView.html#//apple_ref/doc/uid/TP40015214-CH8-SW1

---
+++ ios_sampleapp/ios_sampleapp/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  ios_sampleapp
+//
+//  Created by macmini on 2020/06/22.
+//  Copyright © 2020 hblockth. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
